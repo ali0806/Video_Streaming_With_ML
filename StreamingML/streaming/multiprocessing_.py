dedfc6fd9a4c00a28d0a8ab33d48ba56bf39ec81
Hazrat Ali <85784766+Atik-Hasan-8@users.noreply.github.com>
2023-01-29 19:39:46 +0600

    Update multiprocessing_.py

--- StreamingML/streaming/multiprocessing_.py
+++ StreamingML/streaming/multiprocessing_.py
@@ -8,21 +8,26 @@ from .camera import *
 
 
 def grid():
-    while True:
-        with concurrent.futures.ProcessPoolExecutor(1) as executor:
-            future1=executor.submit(camera1().get_frame)
-            future2=executor.submit(camera2().get_frame)
-            future3=executor.submit(camera3().get_frame)
-            future4=executor.submit(camera4().get_frame)
+	camera1_obj= camera1()
+	camera2_obj= camera2()
+	camera3_obj= camera3()
+	camera4_obj= camera4()
 
-        concat_frame1= cv2.hconcat([future1.result(),future2.result()])
-        concat_frame2= cv2.hconcat([future3.result(),future4.result()])
-        final = cv2.vconcat([concat_frame1,concat_frame2])
-        ret, jpeg = cv2.imencode('.jpg',final)
+    
+	while True:
+		with concurrent.futures.ProcessPoolExecutor(1) as executor:
+			future1=executor.submit(camera1_obj.get_frame)
+			future2=executor.submit(camera2_obj.get_frame)
+			future3=executor.submit(camera3_obj.get_frame)
+			future4=executor.submit(camera4_obj.get_frame)
+	
+		concat_frame1= cv2.hconcat([future1.result(),future2.result()])
+		concat_frame2= cv2.hconcat([future3.result(),future4.result()])
+		final = cv2.vconcat([concat_frame1,concat_frame2])
+		ret, jpeg = cv2.imencode('.jpg',final)
 
-        jpeg=jpeg.tobytes()
-        yield (b'--frame\r\n'
+		jpeg=jpeg.tobytes()
+		yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r\n')
-
 
     
